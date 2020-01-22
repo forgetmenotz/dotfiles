bfe0b58d3169def00067638736fb2391d4d3af6f
forgetmenotz <user@arch.localdomain>
2020-01-22 19:05:17 +0000

    test

---
+++ bin/old/ctrlesc.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+xcape -e 'Control_L=Escape'
+xcape -e 'Mode_switch=colon'
+#xmodmap -e "keycode  53 = x X slash"
+
+#xcape -e 'Mode_switch=colon=Return'
+#xcape -e 'Control_L=Escape'
+#xmodmap -e "keycode  53 = x X slash"
+
